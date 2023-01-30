d2c9561d82280646fd494de060464c96bd776482
Wim Van de Meerssche <wim.vandemeerssche@intec.ugent.be>
2023-01-30 16:23:07 +0100

    fix for sudden crypto bug

--- src/css-flood.ts
+++ src/css-flood.ts
@@ -15,7 +15,7 @@ import { DurationCounter } from "./duration-counter.js";
 import * as fs from "fs";
 import { promises as afs } from "fs";
 import { AccessToken } from "./solid-auth";
-import { getRandomValues } from "node:crypto";
+import { webcrypto } from "node:crypto";
 
 let ya = yargs(hideBin(process.argv))
   .usage("Usage: $0 --url <url> [--steps <steps>] ...")
@@ -220,7 +220,7 @@ function generateUploadData(
   const res = new Uint8Array(uploadSizeByte);
   const startTime = new Date().getTime();
 
-  getRandomValues(res);
+  webcrypto.getRandomValues(res);
   // for (let i = 0; i < uploadSizeByte; i++) {
   //   res[i] = 0;
   // }
